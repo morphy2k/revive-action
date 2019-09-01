941b8b7689d29c963cb0c4aa5e561465dfd05f90
Markus Wiegand <mail@morphy2k.io>
2019-09-01 03:11:04 +0200

    add exclude option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,19 @@
 
 set -e
 
-CONFIG=""
+cd "$GITHUB_WORKSPACE"
+
+IFS=';' read -ra ADDR <<< "$INPUT_EXCLUDE"
+for i in "${ADDR[@]}"; do
+  EXCLUDES="$EXCLUDES -exclude="$i""
+done
 
 if [ ! -z "${INPUT_CONFIG}" ]; then CONFIG="-config=$INPUT_CONFIG"; fi
 
 if [ ! -z "${GITHUB_TOKEN}" ];
 then
-  sh -c "cd $GITHUB_WORKSPACE && revive $CONFIG -formatter ndjson ./... | revive-action"
+  sh -c "revive $CONFIG $EXCLUDES -formatter=ndjson ./... | revive-action"
 else
   echo "Annotations inactive. No GitHub token provided"
-  sh -c "cd $GITHUB_WORKSPACE && revive $CONFIG -formatter friendly ./..."
+  sh -c "revive $CONFIG $EXCLUDES -formatter friendly ./..."
 fi
