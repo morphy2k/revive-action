be1081e0c48f59e39411a2776378a64fae9e7fb6
Markus Wiegand <mail@morphy2k.io>
2019-09-02 03:16:48 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ if [ ! -z "${INPUT_CONFIG}" ]; then CONFIG="-config=$INPUT_CONFIG"; fi
 
 if [ ! -z "${GITHUB_TOKEN}" ];
 then
-  sh -c "revive $CONFIG $EXCLUDES -formatter=ndjson ./... | revive-action"
+  sh -c "revive $CONFIG $EXCLUDES -formatter ndjson ./... | revive-action"
 else
   echo "Annotations inactive. No GitHub token provided"
   sh -c "revive $CONFIG $EXCLUDES -formatter friendly ./..."
