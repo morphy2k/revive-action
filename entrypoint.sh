0fc74bf0de436c82129d83da5ca2321c4f322a2d
Markus Wiegand <mail@morphy2k.dev>
2019-09-26 00:49:02 +0200

    add custom path support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 
 cd "$GITHUB_WORKSPACE"
 
+LINT_PATH="./..."
+
+if [ ! -z "${INPUT_PATH}" ]; then LINT_PATH=$INPUT_PATH; fi
+
 IFS=';' read -ra ADDR <<< "$INPUT_EXCLUDE"
 for i in "${ADDR[@]}"; do
   EXCLUDES="$EXCLUDES -exclude="$i""
@@ -13,8 +17,8 @@ if [ ! -z "${INPUT_CONFIG}" ]; then CONFIG="-config=$INPUT_CONFIG"; fi
 
 if [ ! -z "${GITHUB_TOKEN}" ];
 then
-  sh -c "revive $CONFIG $EXCLUDES -formatter ndjson ./... | revive-action"
+  sh -c "revive $CONFIG $EXCLUDES -formatter ndjson $LINT_PATH | revive-action"
 else
   echo "Annotations inactive. No GitHub token provided"
-  sh -c "revive $CONFIG $EXCLUDES -formatter friendly ./..."
+  sh -c "revive $CONFIG $EXCLUDES -formatter friendly $LINT_PATH"
 fi
